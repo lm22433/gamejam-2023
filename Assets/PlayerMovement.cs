da080f233803fb2aa293c8de83c2a9f07b97f34d
Fin Singleton <finsingleton123@gmail.com>
2023-10-29 20:16:55 +0000

    Level 1 impl

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float moveSpeed = 5f;
+    public float moveSpeed = 100f;
 
     public Rigidbody2D rb;
     public Animator animator;
@@ -22,12 +22,26 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-
-
-
     }
 
     void FixedUpdate(){
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Lvl1Walls"){
+            if (Input.GetKey(KeyCode.LeftArrow)){
+                transform.Translate(0, 0, 0);
+            }
+             if (Input.GetKey(KeyCode.RightArrow)){
+                transform.Translate(0, 0, 0);
+            }
+             if (Input.GetKey(KeyCode.UpArrow)){
+                transform.Translate(0, 0, 0);
+            }
+             if (Input.GetKey(KeyCode.DownArrow)){
+                transform.Translate(0, 0, 0);
+            }
+        }
     }
 }
