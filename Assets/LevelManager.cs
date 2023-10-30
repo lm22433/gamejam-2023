f70db89af144551a1ba4cdac3a0142db2f3a78b5
Harry Greentree <harrygreentree@icloud.com>
2023-10-30 12:05:08 +0000

    ???

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager _instance;
+
+    public bool interact1 = false;
+    public bool interact2 = false;
+    public bool interact3 = false;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (interact1 && interact2 && interact3)
+        {
+            SceneManager.LoadScene(5);
+            Destroy(gameObject);
+        }
+    }
+}
